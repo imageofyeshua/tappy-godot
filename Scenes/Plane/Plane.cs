34fe9758ee608226842c321a473d0250559767c9
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-21 19:54:33 +0900

    Velocity and MoveAndSlide to Plane

---
+++ Scenes/Plane/Plane.cs
@@ -0,0 +1,19 @@
+using System;
+using Godot;
+
+public partial class Plane : CharacterBody2D
+{
+    const float GRAVITY = 800.0f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+        velocity.Y += GRAVITY * (float)delta;
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+}
