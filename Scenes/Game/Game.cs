88406fbcc962544f331496bdc3110493938b3d77
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-23 09:22:54 +0900

    Spawning Pipes Function

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -5,11 +5,24 @@ public partial class Game : Node2D
 {
     [Export] private Marker2D _spawnUpper;
     [Export] private Marker2D _spawnLower;
+    [Export] private Node2D _pipesHolder;
+    [Export] private Timer _spawnTimer;
+    [Export] private PackedScene _pipesScene;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print($"{GetSpawnY()}");
+        // GD.Print($"{GetSpawnY()}");
+        _spawnTimer.Timeout += SpawnPipes;
+
+        SpawnPipes();
+    }
+
+    private void SpawnPipes()
+    {
+        Pipes np = _pipesScene.Instantiate<Pipes>();
+        _pipesHolder.AddChild(np);
+        np.GlobalPosition = new Vector2(_spawnLower.GlobalPosition.X, GetSpawnY());
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,6 +33,6 @@ public partial class Game : Node2D
 
     public float GetSpawnY()
     {
-        return (float)GD.RandRange(_spawnUpper.Position.Y, _spawnLower.Position.Y);
+        return (float)GD.RandRange(_spawnUpper.GlobalPosition.Y, _spawnLower.GlobalPosition.Y);
     }
 }
