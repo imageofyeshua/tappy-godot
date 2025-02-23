4cfa02428f5ebf2d076ca711e1d8c92434ba5273
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-23 20:35:48 +0900

    Space and Q to Main

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -3,12 +3,17 @@ using System;
 
 public partial class Game : Node2D
 {
+    private static readonly PackedScene MAIN_SCENE =
+        GD.Load<PackedScene>("res://Scenes/Main/Main.tscn");
+
     [Export] private Marker2D _spawnUpper;
     [Export] private Marker2D _spawnLower;
     [Export] private Node2D _pipesHolder;
     [Export] private Timer _spawnTimer;
     [Export] private PackedScene _pipesScene;
     [Export] private Plane _plane;
+
+    private bool _gameOver = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -33,6 +38,7 @@ public partial class Game : Node2D
     {
         GD.Print("Game Over");
         StopPipes();
+        _gameOver = true;
     }
 
     private void SpawnPipes()
@@ -45,7 +51,20 @@ public partial class Game : Node2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        if (Input.IsActionJustPressed("fly") && _gameOver)
+        {
+            ChangeToMain();
+        }
 
+        if (Input.IsKeyPressed(Key.Q))
+        {
+            ChangeToMain();
+        }
+    }
+
+    private void ChangeToMain()
+    {
+        GetTree().ChangeSceneToPacked(MAIN_SCENE);
     }
 
     public float GetSpawnY()
